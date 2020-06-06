f5359438e291c50bb1b8ef2af3cb3c22a5f39823
khris22 <khrispunzalan@gmail.com>
2020-06-05 17:49:40 -0700

    added MainContainer as a homepge when logged in

---
+++ src/components/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainContainer = () => {
+    return (
+        <div className="MainContainer">
+        <p>HOMEPAGE when only when logged in!!!</p>
+            {/* main homepage after logging in */}
+        </div>
+    )
+}
+
+export default MainContainer
