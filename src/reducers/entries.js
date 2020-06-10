82466a09371490f3ee7e53a62fcc6868bd50b7c1
khris22 <khrispunzalan@gmail.com>
2020-06-10 14:57:38 -0700

    added SET_ENTRIES reducer

--- src/reducers/entries.js
+++ src/reducers/entries.js
@@ -2,7 +2,8 @@ const initialState = []
 
 export default (state = initialState, action) => {
   switch (action.type) {
-
+    case "SET_ENTRIES":
+      return action.entries
     default:
       return state
   }
