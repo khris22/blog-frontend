52b802b6e7d5c1818922d8c0b060c3683d94bc88
khris22 <khrispunzalan@gmail.com>
2020-06-10 14:57:21 -0700

    added Entries component to MainContainer

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Entries from './Entries'
 
 const MainContainer = () => {
     return (
         <div className="MainContainer">
         <p>HOMEPAGE when only when logged in!!!</p>
+        <Entries />
             {/* main homepage after logging in */}
         </div>
     )
