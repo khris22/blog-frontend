42b6c711e3f3aa082429ae8d11a6e9aa7b2aacbe
khris22 <khrispunzalan@gmail.com>
2020-06-22 11:00:55 -0400

    reset newEntry form

--- src/actions/entries.js
+++ src/actions/entries.js
@@ -1,3 +1,6 @@
+import { resetEntryForm } from './entryForm'
+
+
 // sync
 export const setEntries = entries => {
     return {
@@ -19,7 +22,8 @@ export const addEntry = entry => {
     }
   }
 
-// async
+// async - need something to happen to update the store
+// return a function that returns a promise
   export const getEntries = () => {
     return dispatch => {
       return fetch("http://localhost:3001/api/v1/entries", {
@@ -38,5 +42,38 @@ export const addEntry = entry => {
           }
         })
         .catch(console.log)
+    }
+  }
+
+  export const createEntry = (entryData, history) => {
+    return dispatch => {
+      // use this when formatting does not match with backend
+      // camelCased vs snake_cased
+      const sendableEntryData = {
+        title: entryData.title,
+        notes: entryData.notes,
+        user_id: entryData.userId
+      }
+      return fetch("http://localhost:3001/api/v1/entries", {
+        credentials: "include",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(sendableEntryData)
+      })
+        .then(r => r.json())
+        .then(resp => {
+          if (resp.error) {
+            alert(resp.error)
+          } else {
+            // add trip to redux store, clearing form, going to restful route
+            dispatch(addEntry(resp.data))
+            dispatch(resetEntryForm())
+            history.push(`/entries/${resp.data.id}`)    
+          }
+        })
+        .catch(console.log)
+  
     }
   }
